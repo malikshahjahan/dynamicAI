6bd7f13bdfbded21f1c96283f2a922c57a377305
Malik Shah Jahan <80409870+malikshahjahan@users.noreply.github.com>
2023-04-15 01:28:30 +0500

    First Commit

---
+++ aiprogram.py
@@ -0,0 +1,166 @@
+# -----------------------------Dataset----------------------------------------------------
+import numpy as np
+import pandas as pd
+from matplotlib import pyplot
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
+from sklearn.naive_bayes import GaussianNB
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.svm import SVC
+from sklearn.tree import DecisionTreeClassifier
+
+data = pd.read_csv('weather.csv')
+# convert nominal data to numeric
+from sklearn import preprocessing
+my_label = preprocessing.LabelEncoder()
+data[ 'Precip Type' ] = my_label.fit_transform(data[ 'Precip Type' ])
+print(data[ 'Precip Type' ].unique() )
+print(my_label.inverse_transform(data[ 'Precip Type' ].unique() ))
+feature_names = ['Precip Type', 'Temperature (C)', 'Apparent Temperature (C)', 'Humidity',
+                 'Wind Speed (km/h)', 'Wind Bearing (degrees)', 'Visibility (km)', 'Loud Cover',
+                 'Pressure (millibars)']  # x variable names
+X = data[feature_names]  # setting the col names
+y = data['Summary']  # setting the col names
+target_names = ['Partly Cloudy',
+                'Mostly Cloudy',
+                'Overcast',
+                'Foggy',
+                'Breezy and Mostly Cloudy',
+                'Clear',
+                'Breezy and Partly Cloudy',
+                'Breezy and Overcast',
+                'Humid and Mostly Cloudy',
+                'Humid and Partly Cloudy',
+                'Windy and Foggy',
+                'Windy and Overcast',
+                'Breezy and Foggy',
+                'Windy and Partly Cloudy',
+                'Breezy',
+                'Dry and Partly Cloudy',
+                'Windy and Mostly Cloudy',
+                'Dangerously Windy and Partly Cloudy',
+                'Dry',
+                'Windy',
+                'Humid and Overcast',
+                'Light Rain',
+                'Drizzle',
+                'Windy and Dry',
+                'Dry and Mostly Cloudy',
+                'Breezy and Dry',
+                'Rain']  # potential classes
+data.head(10)
+# print(data[feature_names])
+
+# ----------------------------------Detect Outliers-------------------------------------------
+# box and whisker plots
+data.plot(kind='box', subplots=True, layout=(4, 4), sharex=False, sharey=False)
+pyplot.show()
+
+# histograms
+data.hist()
+pyplot.show()
+
+# # Remove Outliers and clean the data
+# from scipy import stats
+# import numpy as np
+# print(123)
+# z_scores = stats.zscore(data[feature_names])
+# print(123)
+# abs_z_scores = np.abs(z_scores)
+# print(123)
+# filtered_entries = (abs_z_scores < 3).all(axis=1)
+# print(abs_z_scores)
+# clean_df = data[filtered_entries]
+# X = clean_df[feature_names]
+# y = clean_df['Summary']
+# clean_df.head(10)
+# box and whisker plots
+# print(clean_df)
+# clean_df.plot(kind='box', subplots=True, layout=(4, 4), sharex=False, sharey=False)
+# pyplot.show()
+#
+# # histograms
+# clean_df.hist()
+# pyplot.show()
+
+# # Feature Engg
+from sklearn.model_selection import train_test_split, StratifiedKFold, cross_val_score
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20, random_state=0)
+
+# Feature min max scaling
+from sklearn.preprocessing import MinMaxScaler
+
+scaler = MinMaxScaler()
+X_train_scaled = scaler.fit_transform(X_train)
+X_test_scaled = scaler.transform(X_test)
+
+# Spot Check Algorithms
+models = []
+models.append(('LR', LogisticRegression(solver='liblinear', multi_class='ovr')))
+models.append(('LDA', LinearDiscriminantAnalysis()))
+models.append(('KNN', KNeighborsClassifier()))
+models.append(('CART', DecisionTreeClassifier()))
+models.append(('NB', GaussianNB()))
+models.append(('SVM', SVC(gamma='auto')))
+# evaluate each model in turn
+results = []
+names = []
+for name, model in models:
+    kfold = StratifiedKFold(n_splits=10, random_state=1, shuffle=True)
+    cv_results = cross_val_score(model, X_train, y_train, cv=kfold, scoring='accuracy')
+    results.append(cv_results)
+    names.append(name)
+    print('%s: %f (%f)' % (name, cv_results.mean(), cv_results.std()))
+
+# Compare Algorithms
+pyplot.boxplot(results, labels=names)
+pyplot.title('Algorithm Comparison')
+pyplot.show()
+
+# Make predictions on validation dataset
+model = GaussianNB()
+model.fit(X_train, y_train)
+predictions = model.predict(X_test)
+# Evaluate predictions
+print(accuracy_score(y_test, predictions))
+print(confusion_matrix(y_test, predictions))
+print(classification_report(y_test, predictions))
+
+
+# # Fitting into knn
+# from sklearn.neighbors import KNeighborsClassifier, KNeighborsRegressor
+#
+# knn = KNeighborsClassifier(n_neighbors=5)  # setting up the KNN model to use 5NN
+# knn.fit(X_train_scaled, y_train)  # fitting the KNN
+# y_train_pred = knn.predict(X_train_scaled)
+#
+# # Plotting
+# import matplotlib.pyplot as plt
+#
+# error = []
+# for i in range(1, y_test.size):
+#     # knn = KNeighborsClassifier(n_neighbors=i)
+#     knn = KNeighborsRegressor(n_neighbors=i)
+#     knn.fit(X_test, y_test.ravel())
+#     pred_i = knn.predict(X_test)
+#     error.append(np.mean(pred_i != y_test))
+# print("Errors", error)
+# print("KNN Classification Graph (Predicted Data)")
+# plt.style.use('ggplot')
+# plt.subplots(figsize=(10, 6))
+# plt.plot(range(1, y_test.size), error, linestyle='dashed', color='blue', marker='o', markerfacecolor='red')
+# plt.xlabel('K-value')
+# plt.ylabel('Error Rate')
+# plt.title('Error Rate vs K-value')
+# plt.show()
+#
+# # Performance Assessment
+# print('Accuracy of K-NN classifier on training set: {:.2f}'.format(knn.score(X_train_scaled, y_train)))
+# print('Accuracy of K-NN classifier on test set: {:.2f}'.format(knn.score(X_test_scaled, y_test)))
+#
+# # # Prediction
+# test_data = [[0,9.472222222,7.388888889,0.89,14.1197,251,15.8263,0,1015.13]]
+# test_data_scaled = scaler.transform(test_data)
+# print('Predicted SEVERITY_LVL for test_data: ', test_data, ' is ', str(knn.predict(test_data_scaled)[0]))
